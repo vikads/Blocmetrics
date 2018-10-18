02fb9e2584d682f584e1924cccc5f1a571629ff1
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-10-18 14:01:50 -0700

    Allowing users to sign in with their username or email

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ActiveRecord::Base
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable,  and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable,
+         authentication_keys: [:login]
+
+  #to add case insensitivity to your validations on :username
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
+    #f you want email to be case insensitive, you should add
+    conditions[:email].downcase! if conditions[:email]
+    where(conditions.to_hash).first
+end
