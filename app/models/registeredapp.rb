bd7aee01d1529a6d7bbd09e433c31a5c3933d95d
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-10-24 11:18:09 -0700

    4-Register-an-Application-completed

--- app/models/registeredapp.rb
+++ app/models/registeredapp.rb
@@ -1,8 +1,6 @@
 class Registeredapp < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, presence: true
-  validates :url, presence: true
-  validates_uniqueness_of :url
-
+  validates :name, presence: :true
+  validates :url, presence: :true, uniqueness: :true
 end
