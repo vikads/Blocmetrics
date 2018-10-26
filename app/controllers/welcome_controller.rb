44c091d449b62a1811114f48c2759e6a6cdab994
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-10-26 10:54:44 -0700

    deleting pry

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class WelcomeController < ApplicationController
   def index
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class User < ActiveRecord::Base
   attr_writer :login
 
@@ -16,7 +14,7 @@ class User < ActiveRecord::Base
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
 
   def self.find_first_by_auth_conditions(warden_conditions)
-    # binding.pry
+  
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
       where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
