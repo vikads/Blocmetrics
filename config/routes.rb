31be6ae3ce107ae684bd94b1700427ce10b12324
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-10-31 09:14:33 -0700

    to check in curl if events could be added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :registeredapps
 
   namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
     resources :events, only: [:create]
   end
 
